56649f378baad44de1afed68f121348732497238
Igor Souza <igor.souza.webmaster@gmail.com>
2025-04-15 17:22:52 -0300

    feat: improve withdraw styles

--- work-tip/src/app/vault/components/withdraw.tsx
+++ work-tip/src/app/vault/components/withdraw.tsx
@@ -78,7 +78,20 @@ export function WithdrawFromVault({ amount, vaultId }: WithdrawFromVaultProps) {
       onClick={handleWithdraw}
       disabled={amount <= 0 || isLoading}
     >
-      {isLoading ? 'Loading...' : 'Withdraw'}
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <div className="flex items-center justify-center font-semibold ">
+          <span>Withdraw</span>
+          <span className="flex items-center gap-0.5 mx-1">
+            <img
+              src="https://wsrv.nl/?w=128&h=128&default=1&url=https%3A%2F%2Fraw.githubusercontent.com%2Fsolana-labs%2Ftoken-list%2Fmain%2Fassets%2Fmainnet%2FEPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v%2Flogo.png"
+              className="size-4"
+            />
+            <span className="underline">{amount} USDC</span>
+          </span>
+        </div>
+      )}
     </button>
   );
 }
--- work-tip/src/app/vault/page.tsx
+++ work-tip/src/app/vault/page.tsx
@@ -1,6 +1,6 @@
 import { getVaultByUser } from "@/actions/vault";
 import { auth } from "@/auth";
-import { WithdrawFromVault } from "./components/withdraw";
+import { WithdrawContent } from "./components/withdraw-content";
 
 export default async function VaultPage() {
   const session = await auth();
@@ -29,32 +29,11 @@ export default async function VaultPage() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-4 md:p-24">
-      <div className="flex flex-col p-4 bg-zinc-900 rounded-lg">
-        {/* Header */}
-        <div className="flex gap-1 text-xl font-semibold">
-          <div className="flex gap-2 items-center">
-            <img src={vault?.asset.logoUri} className="size-5" />
-            <span>{vault?.asset.symbol}</span>
-          </div>
-          <p>Vault</p>
-        </div>
-
-        {/* Content */}
-        <div className="mt-4 font-medium">
-          <div className="flex gap-2">
-            Withdraw Amount:{" "}
-            <div className="flex gap-2 items-center">
-              <span className="underline font-bold">
-                {vault?.asset.amountParsed} {vault?.asset.symbol}
-              </span>
-            </div>
-          </div>
-          <WithdrawFromVault
-            amount={vault.asset.amountParsed}
-            vaultId={vault.id}
-          />
-        </div>
-      </div>
+      <WithdrawContent
+        assetSymbol={vault.asset.symbol}
+        vaultId={vault.id}
+        withdrawAmount={vault.asset.amountParsed}
+      />
     </main>
   );
 }
